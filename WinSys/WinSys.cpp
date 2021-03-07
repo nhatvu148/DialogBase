ee616fc6743ea3cff0896df542fb0deeaaeadf12
Akiyama <nhatvu148@gmail.com>
2021-03-08 01:02:14 +0900

    copy files

--- WinSys/WinSys.cpp
+++ WinSys/WinSys.cpp
@@ -1,7 +1,31 @@
 #include <Windows.h>
 #include <iostream>
 
+void CreateDirectory();
+void CopyFile();
+
 int main()
+{
+	CopyFile();
+
+	system("PAUSE");
+	return 0;
+}
+
+void CopyFile()
+{
+	BOOL bFile;
+	bFile = CopyFile(L"C:\\Users\\nhatv\\Work\\DialogBase\\WinSys\\OldFile.txt",
+		L"C:\\Users\\nhatv\\Work\\DialogBase\\WinSys\\NewFile.txt",
+		TRUE);
+	if (bFile == FALSE)
+	{
+		std::cout << "CopyFile Failed and Error No - " << GetLastError() << std::endl;
+	}
+	std::cout << "CopyFile Succeeded" << std::endl;
+}
+
+void CreateDirectory()
 {
 	BOOL bDir;
 	bDir = CreateDirectory(L"C:\\Users\\nhatv\\Work\\DialogBase\\WinSys\\Dir1", NULL);
@@ -18,7 +42,4 @@ int main()
 	bDir = RemoveDirectory(L"C:\\Users\\nhatv\\Work\\DialogBase\\WinSys\\Dir1");
 
 	std::cout << "RemoveDirectory Succeeded" << std::endl;
-
-	system("PAUSE");
-	return 0;
 }
